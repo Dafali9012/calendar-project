2fbb924bfdf41f52bc4276f513a619832bdef662
Daniel <Daniel.falklimberg@gmail.com>
2020-10-15 13:33:26 +0200

    component coming along

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -3,38 +3,46 @@ import React from 'react';
 export default function CreateEvent() {
 
     return (
-        <>
-            <div className="row">
-                <div className="col-12 my-4">
-                    <h3>Create Event</h3>
+        <div className="row justify-content-center">
+            <form className="col-6">
+                <h3 className="my-4">Create Event</h3>
+                <div className="form-group">
+                    <label className="w-100"><strong>Title:</strong>
+                        <input className="form-control" name="title" type="text" />
+                    </label>
                 </div>
-                <form className="w-100">
-                    <div className="col-6">
-                        <div className="form-group">
-                            <label className="w-100">Title
-                                <input className="form-control" name="title" type="text"/>
-                            </label>
+                <div className="form-group">
+                    <label className="w-100"><strong>Description:</strong>
+                        <textarea className="form-control" name="description" rows="6" type="text" />
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label className="w-100"><strong>From:</strong>
+                        <div className="row">
+                            <div className="col padr-0">Year<input className="form-control" name="fromYear" type="number" placeholder="ifylld" /></div>
+                            <div className="col padx-0">Month<input className="form-control" name="fromMonth" type="number" placeholder="ifylld" /></div>
+                            <div className="col padl-0">Day<input className="form-control" name="fromDay" type="number" placeholder="ifylld" /></div>
+                            <div className="col padr-0">Hour<input className="form-control" name="fromHour" type="number" placeholder="manuell" /></div>
+                            <div className="col padl-0">Minute<input className="form-control" name="fromMinute" type="number" placeholder="manuell" /></div>
                         </div>
-                        <div className="form-group">
-                            <label className="w-100">Description
-                                <textarea className="form-control" name="description" rows="6" type="text"/>
-                            </label>
+                    </label>
+                </div>
+                <div className="form-group"><strong>To:</strong>
+                    <label className="w-100">
+                        <div className="row">
+                            <div className="col padr-0">Year<input className="form-control" name="toYear" type="number" placeholder="blir ifylld" /></div>
+                            <div className="col padx-0">Month<input className="form-control" name="dateFrom" type="number" placeholder="blir ifylld" /></div>
+                            <div className="col padl-0">Day<input className="form-control" name="dateFrom" type="number" placeholder="blir ifylld" /></div>
+                            <div className="col padr-0">Hour<input className="form-control" name="dateFrom" type="number" placeholder="manuell" /></div>
+                            <div className="col padl-0">Minute<input className="form-control" name="dateFrom" type="number" placeholder="manuell" /></div>
                         </div>
-                    </div>
-                    <div className="col-6">
-                        <div className="form-group">
-                            <label className="w-100">From
-                                <input className="form-control" name="dateFrom" type="number" maxlength="2"/>
-                            </label>
-                        </div>
-                        <div className="form-group">
-                            <label className="w-100">To
-                                <input className="form-control" name="dateTo" type="number"/>
-                            </label>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </>
+                    </label>
+                </div>
+                <div className="float-right">
+                    <button className="btn btn-primary" type="button">Select End Date</button>
+                    <button className="btn btn-primary ml-2" type="submit">Create</button>
+                </div>
+            </form>
+        </div>
     )
 }
