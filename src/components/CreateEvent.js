f40569fc90e2787221a089598532117def04a681
Daniel <Daniel.falklimberg@gmail.com>
2020-10-15 15:08:17 +0200

    layout pretty much complete

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -19,28 +19,57 @@ export default function CreateEvent() {
                 <div className="form-group">
                     <label className="w-100"><strong>From:</strong>
                         <div className="row">
-                            <div className="col padr-0">Year<input className="form-control" name="fromYear" type="number" placeholder="ifylld" /></div>
-                            <div className="col padx-0">Month<input className="form-control" name="fromMonth" type="number" placeholder="ifylld" /></div>
-                            <div className="col padl-0">Day<input className="form-control" name="fromDay" type="number" placeholder="ifylld" /></div>
-                            <div className="col padr-0">Hour<input className="form-control" name="fromHour" type="number" placeholder="manuell" /></div>
-                            <div className="col padl-0">Minute<input className="form-control" name="fromMinute" type="number" placeholder="manuell" /></div>
+                            <div className="col padr-0">Year<input className="form-control" name="fromYear" type="number" disabled /></div>
+                            <div className="col padx-0">Month<input className="form-control" name="fromMonth" type="number" disabled /></div>
+                            <div className="col padl-0">Day<input className="form-control" name="fromDay" type="number" disabled /></div>
+                            <div className="col padr-0">Hour
+                                <select className="form-control" name="fromHour">
+                                    {[...Array(24).keys()].map(num => 
+                                        <option key={num+1} value={num+1}>{num+1}</option>    
+                                    )}
+                                </select>
+                            </div>
+
+                            <div className="col padl-0">Minute
+                                <select className="form-control" name="fromHour">
+                                    {[...Array(11).keys()].map(num =>
+                                        <option key={(num+1)*5} value={(num+1)*5}>{(num+1)*5}</option>
+                                    )}
+                                </select>
+                            </div>
                         </div>
                     </label>
                 </div>
                 <div className="form-group"><strong>To:</strong>
                     <label className="w-100">
                         <div className="row">
-                            <div className="col padr-0">Year<input className="form-control" name="toYear" type="number" placeholder="blir ifylld" /></div>
-                            <div className="col padx-0">Month<input className="form-control" name="dateFrom" type="number" placeholder="blir ifylld" /></div>
-                            <div className="col padl-0">Day<input className="form-control" name="dateFrom" type="number" placeholder="blir ifylld" /></div>
-                            <div className="col padr-0">Hour<input className="form-control" name="dateFrom" type="number" placeholder="manuell" /></div>
-                            <div className="col padl-0">Minute<input className="form-control" name="dateFrom" type="number" placeholder="manuell" /></div>
+                            <div className="col padr-0">Year<input className="form-control" name="toYear" type="number" disabled /></div>
+                            <div className="col padx-0">Month<input className="form-control" name="dateFrom" type="number" disabled /></div>
+                            <div className="col padl-0">Day<input className="form-control" name="dateFrom" type="number" disabled /></div>
+                            <div className="col padr-0">Hour
+                                <select className="form-control" name="fromHour">
+                                    {[...Array(24).keys()].map(num => 
+                                        <option key={num+1}>{num+1}</option>    
+                                    )}
+                                </select>
+                            </div>
+
+                            <div className="col padl-0">Minute
+                                <select className="form-control" name="fromHour">
+                                    {[...Array(11).keys()].map(num =>
+                                        <option key={(num+1)*5}>{(num+1)*5}</option>
+                                    )}
+                                </select>
+                            </div>
                         </div>
                     </label>
                 </div>
                 <div className="float-right">
                     <button className="btn btn-primary" type="button">Select End Date</button>
                     <button className="btn btn-primary ml-2" type="submit">Create</button>
+                </div>
+                <div className="float-left">
+                    <button className="btn btn-danger" type="button">Cancel</button>
                 </div>
             </form>
         </div>
