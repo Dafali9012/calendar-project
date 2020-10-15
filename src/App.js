6fa91f6159cc365f5611e383ad5fca53d3d848f2
David Glane <david@glane.net>
2020-10-15 11:15:59 +0200

    login/register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="h1 text-center">React Calendar</h1>
-     
+    <Router>
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route path="/Login" component={Login} />
+        <Route path="/Register" component={Register} />
+      </Switch>
     </div>
+  </Router>
   );
 }
 
