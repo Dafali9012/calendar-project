2b3815d28b05c58195a44d45ce24d7ab23d736c6
David Glane <david@glane.net>
2020-10-22 13:11:43 +0200

    errorMsg, validate, restruct

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { Alert } from "reactstrap";
 
-export default function Register() {
+export default function Register(props) {
   //this.app = app;
 
   //create state & update values after entering in input
@@ -15,12 +16,17 @@ export default function Register() {
     setState((prevState) => ({ ...prevState, [id]: value }));
   };
 
-  const submitClick = () => {
+  const submitClick = (e) => {
+    e.preventDefault();
     //if passwords match -> registerUser();
     if (state.password === state.passwordConfirm) {
-      registerNewUser();
+      if (state.name.length && state.email.length && state.passwordConfirm.length) {
+        registerNewUser();
+      } else {
+        setShowAlert("Please enter full details");
+      }
     } else {
-      //error message -> password doesn't match
+        setShowAlert("Your password must match");
     }
   };
 
@@ -35,74 +41,91 @@ export default function Register() {
       })
     ).json();
     setState({ email: "", password: "", passwordConfirm: "", name: "" });
+    redirectHome();
   }
 
+  const redirectHome = () => {
+    props.history.push("/");
+  };
+  const [showAlert, setShowAlert] = useState(false);
+
   return (
-    <div className="registerCard d-flex justify-content-center user-select-none">
-      <form>
-        <div className="form-group text-left mt-4">
-          <label className="form-label" htmlFor="email">
-            Email adress
-          </label>
-          <input
-            type="email"
-            className="form-input"
-            id="email"
-            placeholder="dan@domain.com"
-            value={state.email}
-            onChange={updateValues}
-          />
-        </div>
-        <div className="form-group text-left mt-4">
-          <label className="form-label" htmlFor="name">
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-input"
-            id="name"
-            placeholder="name"
-            value={state.name}
-            onChange={updateValues}
-          />
-        </div>
+    <div className="pt-4">
+      <div className="registerCard d-flex justify-content-center user-select-none">
+        <form>
+          <div className="form-group text-left mt-4">
+            <label className="form-label" htmlFor="name">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-input"
+              id="name"
+              placeholder="Dan Smith"
+              value={state.name}
+              onChange={updateValues}
+            />
+          </div>
 
-        <div className="form-group text-left mt-4">
-          <label className="form-label" htmlFor="password">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-input"
-            id="password"
-            placeholder="•••••"
-            value={state.password}
-            onChange={updateValues}
-          />
-        </div>
+          <div className="form-group text-left mt-4">
+            <label className="form-label" htmlFor="email">
+              Email adress
+            </label>
+            <input
+              type="email"
+              className="form-input"
+              id="email"
+              placeholder="dan@domain.com"
+              value={state.email}
+              onChange={updateValues}
+            />
+          </div>
 
-        <div className="form-group text-left mt-4">
-          <label className="form-label" htmlFor="passwordConfirm">
-            Confirm password
-          </label>
-          <input
-            type="password"
-            className="form-input"
-            id="passwordConfirm"
-            placeholder="•••••"
-            value={state.passwordConfirm}
-            onChange={updateValues}
-          />
-        </div>
+          <div className="form-group text-left mt-4">
+            <label className="form-label" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-input"
+              id="password"
+              placeholder="•••••"
+              value={state.password}
+              onChange={updateValues}
+            />
+          </div>
 
-        <button
-          onClick={submitClick}
-          type="submit"
-          className="btn btn-primary submit mt-5"
-        >
-          Register
-        </button>
-      </form>
+          <div className="form-group text-left mt-4">
+            <label className="form-label" htmlFor="passwordConfirm">
+              Confirm password
+            </label>
+            <input
+              type="password"
+              className="form-input"
+              id="passwordConfirm"
+              placeholder="•••••"
+              value={state.passwordConfirm}
+              onChange={updateValues}
+            />
+          </div>
+
+          <button
+            onClick={submitClick}
+            type="submit"
+            className="btn btn-primary submit mt-5"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+      <Alert
+        color="danger"
+        className=""
+        isOpen={showAlert}
+        toggle={() => setShowAlert(false)}
+      >
+        {showAlert}
+      </Alert>
     </div>
   );
 }
