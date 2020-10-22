a0cddf490f3a0fa63a8e86a85250630d467fc2ab
Zero <razviy69@gmail.com>
2020-10-22 15:23:14 +0200

    add-login+session
    Add login/logout + session and cookies

--- src/backend/server/PasswordHandler.js
+++ src/backend/server/PasswordHandler.js
@@ -1,11 +1,14 @@
 const bcrypt = require("bcrypt");
 
 module.exports = class PasswordHandler {
-    
+  
   hashPassword(password) {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt);
     return hash;
-    
+  }
+
+  checkPassword(plaintextPassword, hash) {
+    return bcrypt.compareSync(plaintextPassword, hash);
   }
 };
