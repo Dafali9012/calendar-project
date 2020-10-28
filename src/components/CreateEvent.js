6ded94a8341c63f0537da6fbe768299114274754
Daniel <Daniel.falklimberg@gmail.com>
2020-10-28 16:59:26 +0100

    ...

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -3,6 +3,8 @@ import { Redirect } from 'react-router-dom';
 import {UserContext} from '../Store'
 export default function CreateEvent() {
 
+    // insignificant change
+
     const [formData, setFormData] = useState({
         title:'', description:'',
         fromYear:'', fromMonth:'', fromDay:'', fromHour:'', fromMinute:'',
