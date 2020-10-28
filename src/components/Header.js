f0deb8406378e28066a19db53ce7d28e207a6463
Zero <razviy69@gmail.com>
2020-10-28 20:13:00 +0100

    logout
    Logout + changes on router links on navbar if the user is logged

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,52 @@ import { UserContext } from "../Store";
 
 export default function Header() {
   const [user, setUser] = useContext(UserContext);
+
+  function logout() {
+    return (
+      <Link to="/">
+        <h4 onClick={deleteSession} className="link">Logout</h4>
+      </Link>
+    );
+  }
+
+  async function deleteSession() {
+   let result = await (
+      await fetch("/api/login", {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      })
+    ).json();
+    setUser(null)
+    console.log(result)
+  }
+
+  function login() {
+    return (
+      <Link to="/login">
+        <h4 className="link">Login</h4>
+      </Link>
+    );
+  }
+
+  function register() {
+    return (
+      <Link to="/register">
+        <h4 className="user-btn">Register</h4>
+      </Link>
+    );
+  }
+
   return (
     <header className="header-of-page">
       <div className="col-3 nav-menu">
         <Link to="/">
           <h3 className="link">Calendar</h3>
         </Link>
-        {user == null ? <p>Not user</p> : <p>{"email: ", user.email}</p>}
       </div>
       <div className="header-user col-3">
-        <Link to="/login">
-          <h4 className="link">Login</h4>
-        </Link>
-
-        <Link to="/register">
-          <h4 className="user-btn">Register</h4>
-        </Link>
+        {user == null ? register() : <h4>{user.name}</h4>}
+        {user == null ? login() : logout()}
       </div>
     </header>
   );
