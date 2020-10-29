478adcbf8d14dce0dc9b993c6c6992eee31f858f
Daniel <Daniel.falklimberg@gmail.com>
2020-10-29 22:30:25 +0100

    init revert

--- src/components/DateView.js
+++ src/components/DateView.js
@@ -1,28 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
+import {UserContext} from '../Store';
 
 export default function DateView(){
 
-    /*
-    let mainDate = new Date();
-    let nextDate = new Date();
-    let previousDate = new Date();
-    let dd = String(mainDate.getDate()).padStart(2, '0');
-    let mm = String(mainDate.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yyyy = mainDate.getFullYear();
-    let nextDateDate = String(mainDate.getDate() + 1).padStart(2, '0');
-    let previousDateDate = String(mainDate.getDate() -1).padStart(2, '0');
-    
-    mainDate = yyyy + '-' + dd + '-' + mm;
-    nextDate = yyyy + '-' + nextDateDate + '-' + mm;
-    previousDate = yyyy + '-' + previousDateDate + '-' + mm;
-    */
     useEffect(()=>{
         fetchDateFact(dateSplit);
         fetchEvents();
         // eslint-disable-next-line
     },[])
 
+    // eslint-disable-next-line
+    const [user, setUser] = useContext(UserContext);
     const [redirect, setRedirect] = useState({path:null});
     const [events, setEvents] = useState([]);
     const [dateFact, setDateFact] = useState();
@@ -37,23 +26,14 @@ export default function DateView(){
         setDateFact(data);
     }
 
-    console.log(redirect);
-
     const fetchEvents = async () => {
-        
+        setEvents(await(await fetch("/api/user_event/"+user.id)).json());
     }
 
-    if(redirect.path!=null) {
-        console.log("redirecting");
-        return <Redirect push to={redirect.path}/>
-    }
+    if(redirect.path!=null) return <Redirect push to={redirect.path}/>
     
     let dateToday = new Date();
     let viewDate = new Date();
-
-    dateToday.setHours(0,0,0,0);
-    viewDate.setHours(0,0,0,0);
-
     if(params.date) {
         viewDate = new Date(params.date);
     }
@@ -65,40 +45,41 @@ export default function DateView(){
         dateSplit[x] = dateSplit[x].toString().padStart(2, "0");
     };
 
-    /*
-    //Replace nextDate(date) to mainDate(date)
-    function editmainDateFromPreviousDate(){
-        mainDate = previousDateDate;
-        return mainDate;
+    function previous(){
+        viewDate.setDate(viewDate.getDate()-1)
+        dateSplit = [viewDate.getFullYear(), (viewDate.getMonth()+1), viewDate.getDate()];
+        setRedirect({path:"/date/"+dateSplit[0]+'-'+dateSplit[1]+'-'+dateSplit[2]})
     }
-    */
+    function next(){
+        viewDate.setDate(viewDate.getDate()+1)
+        dateSplit = [viewDate.getFullYear(), (viewDate.getMonth()+1), viewDate.getDate()];
+        setRedirect({path:"/date/"+dateSplit[0]+'-'+dateSplit[1]+'-'+dateSplit[2]})
+    }
 
     return (
         <div className="mt-4">
             <section className="d-flex justify-content-center align-items-center">
-                {/* Knappen finns endast som test så länge datumen inte är klickbara */}
                 
-                <button className="btn-sm btn-primary" onClick={()=>{
-                    setRedirect({path:"/date/1990-12-05"})
-                }}>Previous</button>
+                <button className="btn-sm btn-primary" onClick={prev}>Previous</button>
 
-                {/*<h5 className="col-2 side-date"> {} </h5>*/}
                 <h1 className="text-center mx-5"> {dateSplit.join("-")} </h1>
-                {/*<h5 className="col-2 side-date"> {} </h5>*/}
 
-                {/* Knappen finns endast som test så länge datumen inte är klickbara */}
-                <button className="btn-sm btn-primary">Next</button>
+                <button className="btn-sm btn-primary" onClick={next}>Next</button>
             </section>
             { viewDate.valueOf()>=dateToday.valueOf()?
             <div className="d-flex flex-column align-items-center mt-4">
                 <button className="btn btn-primary" onClick={()=>setRedirect({path:"/date/"+params.date+"/create-event"})}>Create Event</button>
             </div>:null}
-            <h3 className="mt-3 mb-3">Your Events</h3>
+            <h3 className="mt-3 mb-3">Events</h3>
             <div style={{width:"100%", height:"1px", backgroundColor:"black"}}/>
-            <h6 className="mt-4 ml-2">No Events</h6>
             <div>
-            <div className="row justify-content-center align-items-center mt-5"><strong>This date in history:</strong></div>
-        <div className="row justify-content-center align-items-center">{dateFact}</div>
+                {events.map((x,i)=>{
+                    return <div key={i}>{x.userId} {x.eventId} {x.attending}</div>
+                })}
+            </div>
+            <div>
+                <div className="row justify-content-center align-items-center mt-5"><strong>This date in history:</strong></div>
+                <div className="row justify-content-center align-items-center">{dateFact}</div>
             </div>
         </div>
     ) 
