86571ae926be7e89fa2f2be729eae96f251e4b8f
Zero <razviy69@gmail.com>
2020-10-29 11:06:34 +0100

    add-user/event-to-route

--- src/backend/server/RestApi.js
+++ src/backend/server/RestApi.js
@@ -15,10 +15,14 @@ module.exports = class RestApi {
     this.expressApp.post(`${this.routePrefix}/${table}`, (req, res) => {
       // res.json(this.database.select("SELECT * FROM " + table));
       res.json(
-      this.database.run(/*sql*/ `
+        this.database.run(
+          /*sql*/ `
         INSERT INTO ${table} (${Object.keys(req.body)})
         VALUES (${Object.keys(req.body).map((key) => "$" + key)})
-        `, req.body));
+        `,
+          req.body
+        )
+      );
     });
   }
 
@@ -40,34 +44,30 @@ module.exports = class RestApi {
       }
     });
 
-        this.expressApp.get(`/${table}/:id`, (req, res) => {
-          let result = this.database.select(
-            "SELECT * FROM " + table + " WHERE userid = $id",
-            { id: req.params.id }
-          );
-          if (result.length > 0) {
-            res.json(result[0]);
-          } else {
-            res.status(404);
-            res.json({ error: 404 });
-          }
-        });
+    this.expressApp.get(`/user/${table}/:id`, (req, res) => {
+      let result = this.database.select(
+        "SELECT * FROM " + table + " WHERE userid =" + req.params.id
+      );
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.status(404);
+        res.json({ error: 404 });
+      }
+    });
     this.expressApp.get(`${this.routePrefix}/p/${table}/:id`, (req, res) => {
       let result = this.database.select(
         "SELECT password FROM " + table + " WHERE id = $id",
         { id: req.params.id }
       );
       if (result.length > 0) {
-       let pass =  res.json(result[0].password);
-       return pass;
-      }
-      else {
+        let pass = res.json(result[0].password);
+        return pass;
+      } else {
         res.status(404);
         res.json({ error: 404 });
       }
     });
-
-
   }
 
   getAllTables() {
