0a6fb28a6fac64ca6fcd14546bdf5f215db8c559
Anton <anton.ohlin@hotmail.com>
2020-11-02 16:43:57 +0100

    mobileview

--- src/components/DateView.js
+++ src/components/DateView.js
@@ -62,7 +62,7 @@ export default function DateView(){
                 
                 <button className="btn-sm btn-primary" onClick={previous}>Previous</button>
 
-                <h1 className="text-center mx-5"> {dateSplit.join("-")} </h1>
+                <h4 className="text-center mx-5"> {dateSplit.join("-")} </h4>
 
                 <button className="btn-sm btn-primary" onClick={next}>Next</button>
             </section>
@@ -77,9 +77,9 @@ export default function DateView(){
                     return <div key={i} onClick={()=>setRedirect({path:"/event"})}>{x.userId} {x.eventId} {x.attending}</div>
                 })}
             </div>
-            <div>
-                <div className="row justify-content-center align-items-center mt-5"><strong>This date in history:</strong></div>
-                <div className="row justify-content-center align-items-center">{dateFact}</div>
+            <div className="container">
+                <div className="row justify-content-center align-items-center mt-5 ml-1"><strong>This date in history:</strong></div>
+                <div className="row justify-content-center align-items-center text-center">{dateFact}</div>
             </div>
         </div>
     );
