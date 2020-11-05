01b132cb2bc3e91e9778ee9a0c191834bc08b939
David Glane <david@glane.net>
2020-11-05 13:45:41 +0100

    hide menu while !logged

--- src/components/Header.js
+++ src/components/Header.js
@@ -57,29 +57,21 @@ export default function Header() {
     );
   }
 
-  return (
-    <header className="header-of-page">
-      <div className="col-md-3 col-4 nav-menu">
-        <Link className="text-decoration-none" to="/">
-          <h1 className="link logo pl-5">
-            <strong>iCalendar</strong>
-          </h1>
-        </Link>
-      </div>
-
+  function dropIt() {
+    return (
       <div className="header-user col-md-3 col-6">
         <UncontrolledDropdown>
           <DropdownToggle>
-          <div className="row">
+            <div className="row">
               <div className="col-md col-6 my-auto">
                 {user == null ? (
                   <h4 className="button-title userName">Menu</h4>
                 ) : (
-                  <h5 className="userName">
-                    <span role="img" aria-label="user"></span>
-                    {user.name}
-                  </h5>
-                )}
+                    <h5 className="userName">
+                      <span role="img" aria-label="user"></span>
+                      {user.name}
+                    </h5>
+                  )}
               </div>
               {user != null ? (
                 <div className="col-md col-6 my-auto pl-md-0 d-flex justify-content-center">
@@ -90,14 +82,13 @@ export default function Header() {
                   ) : null}
                 </div>
               ) : null}
-              </div>
+            </div>
           </DropdownToggle>
           <DropdownMenu className="drop">
-            <DropdownItem
-            header>
+            <DropdownItem header>
               {user == null ? "I want to: " : user.email}
             </DropdownItem>
-           
+
             {notifications.map((notif) => {
               return (
                 <DropdownItem>
@@ -111,6 +102,19 @@ export default function Header() {
           </DropdownMenu>
         </UncontrolledDropdown>
       </div>
+    );
+  }
+
+  return (
+    <header className="header-of-page">
+      <div className="col-md-3 col-4 nav-menu">
+        <Link className="text-decoration-none" to="/">
+          <h1 className="link logo pl-5">
+            <strong>iCalendar</strong>
+          </h1>
+        </Link>
+      </div>
+      {user == null ? "" : dropIt()}
     </header>
   );
 }
